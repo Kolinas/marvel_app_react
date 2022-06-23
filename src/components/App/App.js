5b000afd7be9bd40493caca6c1e2a37950c76065
Kolinas <konvert99@gmail.com>
2022-06-23 21:38:01 +0300

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,9 @@
+
+
+const App = () => {
+  return (
+    <h1 className="text-blue-600 text-center mt-5 font-bold text-3xl"> My React App Will be here as soon (: </h1>
+  );
+}
+
+export default App;
